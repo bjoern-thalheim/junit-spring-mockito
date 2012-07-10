d08339b340fd23687ecac48af503c6f2cb3f59d4
Björn Thalheim <bjoern.thalheim@gmx.de>
2012-07-10 10:44:13 +0200

    Added a concrete implementation providing EasyMock-Support.

---
+++ src/main/java/com/cellent/spring/utils/junit_spring/SpringEasyMockTest.java
@@ -0,0 +1,22 @@
+package com.cellent.spring.utils.junit_spring;
+
+import org.easymock.EasyMock;
+
+/**
+ * Implementation to provide Easymock Mocks for Classes and Interfaces.
+ * 
+ * @author bjoern
+ */
+public final class SpringEasyMockTest extends AbstractSpringMockTest {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.cellent.spring.utils.junit_spring.AbstractSpringMockTest#
+	 * createMockInstance(java.lang.Class)
+	 */
+	protected <T> T createMockInstance(Class<T> requiredType) {
+		return EasyMock.createMock(requiredType);
+	}
+
+}
